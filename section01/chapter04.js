daf33ee296176f061832b1a1bc2a9bb8fab69036
HwaRang Kim <khrang54@gmail.com>
2025-07-02 12:51:22 +0900

    [Update] CallBack Function update

--- section01/chapter04.js
+++ section01/chapter04.js
@@ -14,11 +14,30 @@ let sub3 = (func, num1, num2) => {
   console.log(num1 / num2);
 };
 
-sub3(sub1, 10, 20);
-console.log("------------------");
-sub3((num1, num2) => console.log(num1 - num2), 10, 20);
+// sub3(sub1, 10, 20);
+// console.log("------------------");
+// sub3((num1, num2) => console.log(num1 - num2), 10, 20);
 
 // 함수 선언문 호이스팅 O
 function add(num1, num2){
   console.log(num1 + num2);
 }
+
+// 콜백함수 응용
+function repeat(count, callback) { 
+  for (let idx = 1; idx <= count; idx++) { 
+    callback(idx); 
+  } 
+} 
+function repeat2(count, callback) { 
+  for (let idx = 1; idx <= count; idx++) { 
+    let result = callback(idx, idx); 
+    console.log(`result = ${result}`);
+  } 
+} 
+// 함수표현식 (화살표 방식)
+repeat2(5, (a, b) => a + b);
+repeat2(5, (a, b) => a - b);
+repeat2(5, (a, b) => a * b);
+repeat2(5, (a, b) => a / b);
+repeat2(5, (a, b) => a * b * 3);
