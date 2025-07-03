dcbf5036ce81187bac239054b2eb0490af5f7fac
HwaRang Kim <khrang54@gmail.com>
2025-07-03 10:30:56 +0900

    [Update] Built-In Array Functions Testing

--- section01/chapter10.js
+++ section01/chapter10.js
@@ -54,7 +54,7 @@ arr.forEach((e) => {
 
 // console.log(`4값위치: ${arr.indexOf(4)}`);
 
-// 배열내장객체 findIndex
+// 배열내장객체 findIndex (true, false)
 const resultIndex = arr.findIndex((e) => {
   // console.log("찾는중임");
   return e === 4;
@@ -81,4 +81,48 @@ const resultArray2 = arr6.filter((e) => {
 const resultArray3 = arr6.map((e) => {
   return e.hobby + e.name;
 });
-console.log(resultArray3);
+// console.log(resultArray3);
+
+// 배열내장객체 slice
+let arr7 = [
+  { name: "구길동", hobby: "테니스" },
+  { name: "저길동", hobby: "테니스" },
+  { name: "홍길동", hobby: "독서" },
+  { name: "홍길동", hobby: "독서" },
+  { name: "홍길동", hobby: "독서" },
+];
+// console.log(arr7.slice(0, 4));
+
+// 배열내장객체 concat
+let arr8 = [
+  { name: "구길동", hobby: "테니스" },
+  { name: "저길동", hobby: "테니스" },
+];
+let arr9 = [
+  { name: "홍길동", hobby: "독서" },
+  { name: "홍길동", hobby: "독서" },
+];
+
+// console.log(arr8.concat(arr9));
+
+// 배열내장객체 sort
+let charArray = ["가", "라", "다", "나"];
+// console.log(charArray.sort());
+
+let numArray = [1, 20, 11, 34, 50, 15, 100];
+// console.log(numArray.sort());
+
+//배열내장객체 sort 역정렬, 정렬 (기준)
+console.log(numArray);
+numArray.sort((a, b) => a - b);
+console.log(numArray);
+
+// 배열내장객체 toSorted 역정렬, 정렬 (기준)
+const sortedArray = numArray.toSorted((a, b) => b - a);
+console.log(numArray);
+console.log(sortedArray);
+
+// 배열내장객체 join
+const arr10 = ["김동진", "님", "안녕하세요", "반가워요"];
+const resultStr = arr10.join();
+console.log(resultStr);
